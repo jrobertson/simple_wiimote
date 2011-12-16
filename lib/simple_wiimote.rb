7c88c0cbec14cc1557380b759830d95f8017873d
Your Name <james@r0bertson.co.uk>
2011-12-16 11:23:26 +0000

    fixed the timing button press ignore bug

--- lib/simple_wiimote.rb
+++ lib/simple_wiimote.rb
@@ -4,9 +4,7 @@
 
 require 'cwiid'
 
-class WiiMote
-  attr_accessor :active
-end
+class WiiMote  attr_accessor :active  end
 
 class SimpleWiimote
 
@@ -15,22 +13,24 @@ class SimpleWiimote
   def initialize()
 
     puts 'Put Wiimote in discoverable mode now (press 1+2)...'
+    
     @wiimote = WiiMote.new
     @wiimote.rpt_mode = WiiMote::RPT_BTN | WiiMote::RPT_ACC
-
     @wiimote.active = false
-    @events = {}
-    @events[WiiMote::BTN_PLUS] = -> {puts 'you pressed plus'}
-    @events[WiiMote::BTN_UP] =   -> {puts 'you pressed up'}
-    @events[WiiMote::BTN_DOWN] = -> {puts 'you pressed down'}
-    @events[WiiMote::BTN_RIGHT]  =-> {puts 'you pressed right'}
-    @events[WiiMote::BTN_LEFT] = -> {puts 'you pressed left'}
-    @events[WiiMote::BTN_HOME] = -> {puts 'you pressed home'}
-    @events[WiiMote::BTN_MINUS] = -> {puts 'you pressed minus'}
-    @events[WiiMote::BTN_A] = -> {puts 'you pressed a'}
-    @events[WiiMote::BTN_B] = -> {puts 'you pressed b'}
-    @events[WiiMote::BTN_1] = -> {puts 'you pressed 1'}
-    @events[WiiMote::BTN_2] = -> {puts 'you pressed 2'}
+    
+    @events = {
+      WiiMote::BTN_PLUS =>  -> {puts 'you pressed plus'},
+      WiiMote::BTN_UP   =>  -> {puts 'you pressed up'},
+      WiiMote::BTN_DOWN =>  -> {puts 'you pressed down'},
+      WiiMote::BTN_RIGHT => -> {puts 'you pressed right'},
+      WiiMote::BTN_LEFT =>  -> {puts 'you pressed left'},
+      WiiMote::BTN_HOME =>  -> {puts 'you pressed home'},
+      WiiMote::BTN_MINUS => -> {puts 'you pressed minus'},
+      WiiMote::BTN_A    =>  -> {puts 'you pressed a'},
+      WiiMote::BTN_B    =>  -> {puts 'you pressed b'},
+      WiiMote::BTN_1    =>  -> {puts 'you pressed 1'},
+      WiiMote::BTN_2    =>  -> {puts 'you pressed 2'}
+    }
 
     @terminator = WiiMote::BTN_A
   end
@@ -42,32 +42,15 @@ class SimpleWiimote
       @wiimote.active = true
       yield(@wiimote) if block_given?  
 
-      old_btn ||= @wiimote.buttons
       if @wiimote.buttons > 0  then
-        if @wiimote.buttons != old_btn then
-          r = @events[@wiimote.buttons]
-          (old_btn = 0; r.call; sleep 0.2) if r
-          old_btn = @wiimote.buttons
-
-        else
-          sleep 0.2
-          old_btn = 0
-        end
+	r = @events[@wiimote.buttons]
+	sleep 0.2
+	r.call  if r
       end
     end until (@wiimote.buttons & @terminator ) != 0  || !@wiimote.active 
   end
 
-  def led=(val)
-    @led = val
-    @wiimote.led = val
-  end
-
-  def rumble=(bool)
-    @rumble = bool
-    @wiimote.rumble = bool
-  end
-
-  def close()
-    @wiimote.close
-  end
+  def led=(val)      @led    = @wiimote.led    = val   end
+  def rumble=(bool)  @rumble = @wiimote.rumble = bool  end
+  def close()        @wiimote.close  end
 end
